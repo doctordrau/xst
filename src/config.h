12b25ff55394baf28199740e01416bf38b29ecac
doctordrau <doctordrau@yahoo.com>
2017-10-10 03:26:15 +0300

    Update config.h

--- src/config.h
+++ src/config.h
@@ -102,7 +102,7 @@ static unsigned int defaultrcs = 259;*/
 
 *static unsigned int defaultfg = 258;
 static unsigned int defaultbg = 256;
-static unsigned int defaultcs = 258;
+static unsigned int defaultcs = 259;
 static unsigned int defaultrcs = 257;		
 
 /* 2 4 6 7: █ _ | ☃ */
