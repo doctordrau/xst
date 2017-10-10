78ccf9d80bdf7926f5659ee0886e65d8b7a771c8
doctordrau <doctordrau@yahoo.com>
2017-10-10 03:07:12 +0300

    Update config.h

--- src/config.h
+++ src/config.h
@@ -82,19 +82,28 @@ static char *colorname[] = {
 
 	[255] = 0,
 
-	"#a7a7a7", 		/* [default] defaultfg */
-	"#1e1e1e",		/* [default] defaultbg */
-	"#a7a7a7",              /* [default] defaultcs */
-	"#1e1e1e",		/* [default] defaultrcs */
-	"black",
+	"#000000",  /* 256: background */
+        "#ffffff",  /* 258: background */ 
+        "#fbfbfb",  /* 257: foreground */
+	
+	/*"#a7a7a7", 		/* [default] defaultfg */
+	/*"#1e1e1e",		/* [default] defaultbg */
+	/*"#a7a7a7",              /* [default] defaultcs */
+	/*"#1e1e1e",		/* [default] defaultrcs */
+	/*"black",
 };
 
 
 /* fg, bg, cursor, reverse cursor (references colorname indexes) */
-static unsigned int defaultfg = 256;
+/*static unsigned int defaultfg = 256;
 static unsigned int defaultbg = 257;
 static unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 259;
+static unsigned int defaultrcs = 259;*/
+
+static unsigned int defaultfg = 258;
+static unsigned int defaultbg = 256;
+static unsigned int defaultcs = 14;
+static unsigned int defaultrcs = 257;		
 
 /* 2 4 6 7: █ _ | ☃ */
 static unsigned int cursorshape = 6;
