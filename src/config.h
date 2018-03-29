443e0d82517165cbc9b96ce14539fcffcfc3295f
doctordrau <doctordrau@yahoo.com>
2018-03-30 02:27:02 +0300

    Update config.h

--- src/config.h
+++ src/config.h
@@ -125,11 +125,11 @@ static unsigned int defaultcs = 14;
 static unsigned int defaultrcs = 257;*/		
 
 /* 2 4 6 7: █ _ | ☃ */
-static unsigned int cursorshape = 7;
+static unsigned int cursorshape = 4;
 
 /* 0: normal blinking
  * 1: leave cursor border and blink with cursor's background */
-static unsigned int cursorblinkstyle = 0;
+static unsigned int cursorblinkstyle = 1;
 
 /* 0: cursor blinks with a constant interval
  * 1: blinking cycle resets on key input */
